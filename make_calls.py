5a31df97234685ccefcfc24aba6c256ed4614bbe
Eric Zorn <zornwebdev@gmail.com>
2026-01-24 13:54:40 -0500

    feat: calls

--- make_calls.py
+++ make_calls.py
@@ -1,5 +1,6 @@
 import asyncio
 import http
+import time
 
 import httpx
 
@@ -10,8 +11,10 @@ async def make_call(i) -> int:
     async with _sem:
         async with httpx.AsyncClient() as client:
             print(f"Making call {i}")
-            response = await client.get("https://randompy.fly.dev/healthz")
-            print(response.status_code)
+            start = time.perf_counter()
+            response = await client.get("https://randompy.fly.dev/todos")
+            end = time.perf_counter()
+            print(f"Call {i} completed in {end - start:.2f} seconds")
 
         await asyncio.sleep(0.1)
 
@@ -19,11 +22,12 @@ async def make_call(i) -> int:
 
 
 async def main() -> None:
+    start = time.perf_counter()
     tasks = [asyncio.create_task(make_call(i)) for i in range(1_000)]
     statuses = await asyncio.gather(*tasks)
+    end = time.perf_counter()
     success_codes = [status for status in statuses if status == http.HTTPStatus.OK]
-
-    print(f"All calls completed with success codes: {len(success_codes)}")
+    print(f"All calls completed with success codes: {len(success_codes)} in {end - start:.2f} seconds")
 
 
 if __name__ == "__main__":
